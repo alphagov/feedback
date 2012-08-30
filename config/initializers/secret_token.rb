b4d3e98d3a40fbb341ef8ab13a8c46551e7ac4b4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-08-30 13:24:21 +0000

    Blank Rails 3.2.8 app
    rails new feedback -O -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedback::Application.config.secret_token = 'b3c23cd42026eaae291d3a55bbe609338c03b2efcef78137cca41ce966571aa1dc513dbd70de278d497a3948dd8dd49e0e63d65dc0e8c3e1c7abeac656cfdc36'
