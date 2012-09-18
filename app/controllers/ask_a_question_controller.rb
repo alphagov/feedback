9cc6281f52a7b505d68b8341dd35e3f3ec9fa1e0
Magnus Ernstsson <magnus@ernstsson.net>
2012-09-18 15:09:02 +0100

    Added basic validation functions to ask a question controller/view

--- app/controllers/ask_a_question_controller.rb
+++ app/controllers/ask_a_question_controller.rb
@@ -10,18 +10,46 @@ class AskAQuestionController < ApplicationController
   end
 
   def submit
-    description = format_description params
-    result = @@ticket_client.raise_ticket({
-      subject: "Ask a question",
-      tags: ["ask_question"],
-      name: params[:name],
-      email: params[:email],
-      department: params[:section],
-      description: description});
-      render :action => "thankyou"
+    @errors = Validator.validate('ask_a_question', params)
+    puts @errors
+    if @errors.empty?
+      description = format_description params
+      result = @@ticket_client.raise_ticket({
+        subject: "Ask a question",
+        tags: ["ask_question"],
+        name: params[:name],
+        email: params[:email],
+        department: params[:section],
+        description: description});
+        render :action => "thankyou"
+    else
+      @departments = @@ticket_client.get_departments
+      render :action => "landing"
+    end
   end
 
   private
+
+  def validate_form(params)
+  end
+
+  class Validator
+    @@required = { 'ask_a_question' => ['name', 'email']}
+
+    def self.validate(form_name, params)
+      errors = []
+      errors = validate_required form_name, params, errors
+    end
+
+    def self.validate_required(form_name, params, errors)
+      @@required[form_name].each do |key|
+        if params[key].blank?
+          errors << "#{key} is blank"
+        end
+      end
+      errors
+    end
+  end
 
   def format_description(params)
     description = @@QUESTION_TITLE + "\n" + params[:question]
@@ -30,5 +58,4 @@ class AskAQuestionController < ApplicationController
     end
     description
   end
-
 end
