0d9a9055d9226ea8e70cfa9d5b4b7d3ae7d9e0eb
Magnus Ernstsson <magnus@ernstsson.net>
2012-09-21 14:21:17 +0100

    Made ticket client more robust

--- lib/ticket_client.rb
+++ lib/ticket_client.rb
@@ -1,5 +1,5 @@
 class TicketClient
-  @@DEPARTMENT_FIELD = 21494928
+  DEPARTMENT_FIELD = 21494928
 
   class << self
 
@@ -9,7 +9,7 @@ class TicketClient
         :subject => zendesk[:subject],
         :tags => tags,
         :requester => {name: zendesk[:name], email: zendesk[:email]},
-        :fields => [{id: @@DEPARTMENT_FIELD, value: zendesk[:department]}],
+        :fields => [{id: DEPARTMENT_FIELD, value: zendesk[:department]}],
         :description => zendesk[:description]
       )
       !! result
@@ -17,22 +17,26 @@ class TicketClient
 
     def get_departments
       departments_hash = {}
-      department_field = client.ticket_fields.find(:id => @@DEPARTMENT_FIELD)
-      department_field.custom_field_options.each do |tf| 
-        departments_hash[tf.name] = tf.value
+      unless client.nil?
+        department_field = client.ticket_fields.find(:id => DEPARTMENT_FIELD)
+        unless department_field.nil?
+          department_field.custom_field_options.each do |tf| 
+            departments_hash[tf.name] = tf.value
+          end
+        end
       end
       departments_hash
     end
 
     def client 
-      @@client ||= build_client
+      @client ||= build_client
     end
 
     private
 
     def build_client
       details = YAML.load_file(Rails.root.join('config', 'zendesk.yml'))
-      @@client = ZendeskAPI::Client.new do |config|
+      @client = ZendeskAPI::Client.new do |config|
         config.url = details["url"]
         config.username = details["username"]
         config.password = details["password"]
