30926310978adb889a3dfb12c430ad899bea4237
Magnus Ernstsson <magnus@ernstsson.net>
2012-09-24 16:14:25 +0100

    Fixed IE js problems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,24 +3,22 @@ var GOVUK = GOVUK || {};
 GOVUK.feedback = {};
 
 GOVUK.feedback.checkEmail = function (input) {
-  if (input.value != $('email').value) {
-    input.setCustomValidity('The two email addresses must match.');
-  } else {
-    input.setCustomValidity('');
+  if (input.setCustomValidity) {
+    if (input.value != $('email').val()) {
+      input.setCustomValidity('The two email addresses must match.');
+    } else {
+      input.setCustomValidity('');
+    }
   }
 };
 
 GOVUK.feedback.checkOnInputEmail = function () {
-  alert('hej');
   this.onkeydown = null;
   GOVUK.feedback.checkEmail(this);
-  alert('hej2');
 };
 
 GOVUK.feedback.checkOnKeyDownEmail = function () {
-  alert('hopp');
   GOVUK.feedback.checkEmail(this);
-  alert('hopp2');
 };
 
 GOVUK.feedback.initUserDetails = function () {
