f113641b4abd4ba61f9b308e0b71718a136a4701
Magnus Ernstsson <magnus@ernstsson.net>
2012-10-08 10:16:47 +0100

    Changed report a problem to use active model validation

--- app/models/report_a_problem_ticket.rb
+++ app/models/report_a_problem_ticket.rb
@@ -1,44 +1,42 @@
-class ReportAProblemTicket
-  def initialize(params)
-    @params = params
-    @errors = {}
+class ReportAProblemTicket < Ticket
+
+  attr_accessor :what_wrong, :what_doing, :url, :controller, :action
+
+  def initialize(attributes = {})
+    attributes.each do |key, value|
+      send("#{key}=", value)
+    end
+    valid?
   end
 
   def save
+    ticket = nil
     begin
-      ticket = report_a_problem_ticket @params
+      ticket = report_a_problem_ticket
       ticket_client.raise_ticket(ticket)
-      true
     rescue => e
-      @errors[:connection] = "Connection error"
+      ticket = nil
+      @errors.add :connection, "Connection error"
       ExceptionNotifier::Notifier.background_exception_notification(e).deliver
-      false
     end
-  end
-
-  def errors
-    @errors
+    ticket
   end
 
   private
 
-  def ticket_client
-    @ticket_client ||= TicketClientConnection.get_client
-  end
-
-  def report_a_problem_ticket(params)
-    description = report_a_problem_format_description params
+  def report_a_problem_ticket
+    description = report_a_problem_format_description
     ticket = {
-      :subject => path_for_url(params[:url]),
+      :subject => path_for_url(url),
       :tags => ['report_a_problem'],
       :description => description
     }
   end
 
-  def report_a_problem_format_description(params)
-    description = "url: #{params[:url]}\n" +
-    "what_doing: #{params[:what_doing]}\n" +
-    "what_wrong: #{params[:what_wrong]}"
+  def report_a_problem_format_description
+    description = "url: #{url}\n" +
+    "what_doing: #{what_doing}\n" +
+    "what_wrong: #{what_wrong}"
   end
 
   def path_for_url(url)
