df44345ec067211c85f8a9f6e5b640363693d347
Jordan Hatch <jordan@jordanh.net>
2012-11-16 10:49:00 +0000

    Ensure ie6 stylesheet is precompiled

--- config/application.rb
+++ config/application.rb
@@ -69,7 +69,7 @@ module Feedback
     config.assets.prefix = 'feedback' # this has to match the path configured in puppet and deploy scripts.
 
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w(feedback.js feedback.css)
+    config.assets.precompile += %w(feedback.js feedback.css feedback-ie6.css)
 
     # Disable Rack::Cache
     config.action_dispatch.rack_cache = nil
