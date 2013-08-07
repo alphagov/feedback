a2651dd81c31e563470e21af57edcead3fe98099
Jake Benilov <benilov@gmail.com>
2013-08-07 10:42:46 +0000

    reduce duplication in FOI spec

--- spec/models/foi_ticket_spec.rb
+++ spec/models/foi_ticket_spec.rb
@@ -1,95 +1,59 @@
 require 'spec_helper'
+require 'rspec/expectations'
+
+RSpec::Matchers.define :have_errors_on do |expected_attribute_name|
+  match do |model|
+    model.errors.has_key?(expected_attribute_name)
+  end
+end
 
 describe FoiTicket do
   include ValidatorHelper
 
-  it "should return no errors on valid params" do
-    test_data = {
-        name: "test name",
-        email: "a@a.com",
-        email_confirmation: "a@a.com",
-        textdetails: "test foi"
+  def valid_foi_request_defaults
+    {
+      name: "test name",
+      email: "a@a.com",
+      email_confirmation: "a@a.com",
+      textdetails: "test foi"
     }
-    ticket = FoiTicket.new test_data
-    ticket.valid?.should eq true
+  end
+
+  def foi_request(options = {})
+    FoiTicket.new(valid_foi_request_defaults.merge(options))
+  end
+
+  it "should return no errors on valid params" do
+    foi_request.should be_valid
   end
 
   it "should return email error with invalid email" do
-    test_data = {
-        name: "test name",
-        email: "a",
-        email_confirmation: "a@a",
-        textdetails: "test foi"
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :email).should eq true
+    foi_request(email: "a").should have_errors_on(:email)
   end
 
   it "should return email error with empty email" do
-    test_data = {
-        name: "test name",
-        email: "",
-        email_confirmation: "a@a",
-        textdetails: "test foi"
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :email).should eq true
+    foi_request(email: "").should have_errors_on(:email)
   end
 
   it "should return email error with non matching verification email" do
-    test_data = {
-        name: "test name",
-        email: "a@a",
-        email_confirmation: "a@b",
-        textdetails: "test foi"
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :email).should eq true
+    foi_request(email: "a@a", email_confirmation: "a@b").should have_errors_on(:email)
   end
 
   it "should return name error with empty name" do
-    test_data = {
-        name: "",
-        email: "a@a",
-        email_confirmation: "a@a",
-        textdetails: "test foi"
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :name).should eq true
+    foi_request(name: "").should have_errors_on(:name)
   end
 
   it "should return foi error with empty textdetails" do
-    test_data = {
-        name: "test name",
-        email: "a@a",
-        email_confirmation: "a@a",
-        textdetails: ""
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :textdetails).should eq true
+    foi_request(textdetails: "").should have_errors_on(:textdetails)
   end
 
   it "should return foi error with too long foi text" do
-    textdetails = build_random_string 1251
-    test_data = {
-        name: "test name",
-        email: "a@a",
-        email_confirmation: "a@a",
-        textdetails: textdetails
-    }
-    ticket = FoiTicket.new test_data
-    (ticket.errors.has_key? :textdetails).should eq true
+    foi_request(textdetails: build_random_string(1251)).should have_errors_on(:textdetails)
   end
 
   it "should raise an exception if zendesk ticket creation fails" do
-    test_data = {
-         name: "test name",
-         email: "a@a.com",
-         email_confirmation: "a@a.com",
-         textdetails: "test foi"
-     }
-     ticket = FoiTicket.new test_data
-     ticket.stub(:ticket_client).and_raise('some error')
-     lambda { ticket.save }.should raise_error('some error')
+    ticket = foi_request
+    ticket.stub(:ticket_client).and_raise('some error')
+    lambda { ticket.save }.should raise_error('some error')
   end
 end
