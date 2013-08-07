472f3474613f024451c73987f5e8dcbc895c2579
Jake Benilov <benilov@gmail.com>
2013-08-07 13:58:51 +0000

    replacing custom matcher with one from rspec-rails

--- spec/models/foi_ticket_spec.rb
+++ spec/models/foi_ticket_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
-require 'rspec/expectations'
-
-RSpec::Matchers.define :have_errors_on do |expected_attribute_name|
-  match do |model|
-    model.errors.has_key?(expected_attribute_name)
-  end
-end
 
 describe FoiTicket do
   include ValidatorHelper
@@ -28,27 +21,27 @@ describe FoiTicket do
   end
 
   it "should return email error with invalid email" do
-    foi_request(email: "a").should have_errors_on(:email)
+    foi_request(email: "a", email_confirmation: "a").should have(1).error_on(:email)
   end
 
   it "should return email error with empty email" do
-    foi_request(email: "").should have_errors_on(:email)
+    foi_request(email: "", email_confirmation: "").should have_at_least(1).error_on(:email)
   end
 
   it "should return email error with non matching verification email" do
-    foi_request(email: "a@a", email_confirmation: "a@b").should have_errors_on(:email)
+    foi_request(email: "a@a.com", email_confirmation: "a@b.com").should have(1).error_on(:email)
   end
 
   it "should return name error with empty name" do
-    foi_request(name: "").should have_errors_on(:name)
+    foi_request(name: "").should have(1).error_on(:name)
   end
 
   it "should return foi error with empty textdetails" do
-    foi_request(textdetails: "").should have_errors_on(:textdetails)
+    foi_request(textdetails: "").should have(1).error_on(:textdetails)
   end
 
   it "should return foi error with too long foi text" do
-    foi_request(textdetails: build_random_string(1251)).should have_errors_on(:textdetails)
+    foi_request(textdetails: build_random_string(1251)).should have(1).error_on(:textdetails)
   end
 
   it "should raise an exception if zendesk ticket creation fails" do
