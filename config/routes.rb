0070f76baf9b2058ff2a9519df51688867350253
Amy Whitney <amy.whitney@digital.cabinet-office.gov.uk>
2013-10-11 14:15:32 +0000

    Add /contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,16 @@ Feedback::Application.routes.draw do
   get "/feedback/foi", :to => "feedback#foi", :format => false
   post "/feedback/foi", :to => "feedback#foi_submit", :format => false
 
+  # Feedback routes are moving to the /contact routes below.
+  # The above routes can be safely removed once redirects are in place
+  # for the old /feedback URLs.
+  get "/contact", :to => "feedback#feedback", :format => false
+  post "/contact", :to => "feedback#report_a_problem_submit", :format => false
+  get "/contact/form", :to => "feedback#contact", :format => false
+  post "/contact/form", :to => "feedback#contact_submit", :format => false
+  get "/contact/foi", :to => "feedback#foi", :format => false
+  post "/contact/foi", :to => "feedback#foi_submit", :format => false
+
   root :to => redirect("/feedback", :status => 302)
 
   if Rails.env.development? or Rails.env.test?
