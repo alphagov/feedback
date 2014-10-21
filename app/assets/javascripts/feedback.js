7dddfcf849b847af54a8d8979a8083ea756db144
Edd Sowden <e@e26.co.uk>
2014-10-21 12:06:55 +0000

    Fix JavaScript error in IE8 by wrapping code
    There was an 'script error' in IE8 which has something to do with the
    way it interprets the compiled/minified JavaScript. Wrapping the
    application specific code in an anonymous function fixes this.

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,84 +1,89 @@
 //= require jquery.details.js
 
-var GOVUK = GOVUK || {};
+(function(){
+  "use strict";
 
-GOVUK.feedback = {};
+  window.GOVUK = GOVUK || {};
 
-GOVUK.feedback.checkEmail = function (input) {
-    if (input.setCustomValidity) {
-        if (input.value != $('#email').val()) {
-            input.setCustomValidity('The two email addresses must match.');
-        } else {
-            input.setCustomValidity('');
-        }
-    }
-};
+  GOVUK.feedback = {};
 
-GOVUK.feedback.checkOnInputEmail = function () {
-    this.onkeydown = null;
-    GOVUK.feedback.checkEmail(this);
-};
+  GOVUK.feedback.checkEmail = function (input) {
+      if (input.setCustomValidity) {
+          if (input.value != $('#email').val()) {
+              input.setCustomValidity('The two email addresses must match.');
+          } else {
+              input.setCustomValidity('');
+          }
+      }
+  };
 
-GOVUK.feedback.checkOnKeyDownEmail = function () {
-    GOVUK.feedback.checkEmail(this);
-};
+  GOVUK.feedback.checkOnInputEmail = function () {
+      this.onkeydown = null;
+      GOVUK.feedback.checkEmail(this);
+  };
 
-GOVUK.feedback.initUserDetails = function () {
-    $('#verifyemail').on('input', GOVUK.feedback.checkOnInputEmail);
-    $('#verifyemail').on('keydown', GOVUK.feedback.checkOnKeyDownEmail);
-}
+  GOVUK.feedback.checkOnKeyDownEmail = function () {
+      GOVUK.feedback.checkEmail(this);
+  };
 
-GOVUK.feedback.handleCounter = function (counted) {
-    var counterId = '#' + counted.id + 'counter';
-    $(counterId).html((1200 - counted.value.length) +  " characters remaining (limit is 1200 characters)");
-}
+  GOVUK.feedback.initUserDetails = function () {
+      $('#verifyemail').on('input', GOVUK.feedback.checkOnInputEmail);
+      $('#verifyemail').on('keydown', GOVUK.feedback.checkOnKeyDownEmail);
+  }
+
+  GOVUK.feedback.handleCounter = function (counted) {
+      var counterId = '#' + counted.id + 'counter';
+      $(counterId).html((1200 - counted.value.length) +  " characters remaining (limit is 1200 characters)");
+  }
 
 
-GOVUK.feedback.initCounters = function () {
-    $('.counted').each(function (index) {
-        this.oninput = function () {
-            this.onkeydown = null;
-            GOVUK.feedback.handleCounter(this);
-        };
+  GOVUK.feedback.initCounters = function () {
+      $('.counted').each(function (index) {
+          this.oninput = function () {
+              this.onkeydown = null;
+              GOVUK.feedback.handleCounter(this);
+          };
 
-        this.onkeydown = function () {
-            GOVUK.feedback.handleCounter(this);
-        };
-    });
-}
+          this.onkeydown = function () {
+              GOVUK.feedback.handleCounter(this);
+          };
+      });
+  }
 
-GOVUK.feedback.checkRadio = function () {
-    if ($('#location-specific').is(':checked')) {
-        $('#link').removeAttr('disabled');
-    } else {
-        $('#link').attr('disabled', 'disabled')
-    }
-}
+  GOVUK.feedback.checkRadio = function () {
+      if ($('#location-specific').is(':checked')) {
+          $('#link').removeAttr('disabled');
+      } else {
+          $('#link').attr('disabled', 'disabled')
+      }
+  }
 
-GOVUK.feedback.prepopulateFormBasedOnReferrer = function () {
-    $('#link').val(document.referrer);
-}
+  GOVUK.feedback.prepopulateFormBasedOnReferrer = function () {
+      $('#link').val(document.referrer);
+  }
 
-GOVUK.feedback.init = function () {
-    GOVUK.feedback.initCounters();
-    GOVUK.feedback.initUserDetails();
-    GOVUK.feedback.checkRadio();
+  GOVUK.feedback.init = function () {
+      GOVUK.feedback.initCounters();
+      GOVUK.feedback.initUserDetails();
+      GOVUK.feedback.checkRadio();
 
-    $('button.button').click(function() {
-        $(this).attr('disabled', 'disabled');
-        $(this).parents('form').submit();
-    });
+      $('button.button').click(function() {
+          $(this).attr('disabled', 'disabled');
+          $(this).parents('form').submit();
+      });
 
-    $('fieldset#location').change(function () {
-        GOVUK.feedback.checkRadio();
-    });
+      $('fieldset#location').change(function () {
+          GOVUK.feedback.checkRadio();
+      });
 
-    GOVUK.feedback.prepopulateFormBasedOnReferrer();
-    $('form.contact-form').append('<input type="hidden" name="contact[javascript_enabled]" value="true"/>');
-    $('form.contact-form').append('<input type="hidden" name="contact[referrer]" value="' + document.referrer + '"/>');
+      GOVUK.feedback.prepopulateFormBasedOnReferrer();
+      $('form.contact-form').append('<input type="hidden" name="contact[javascript_enabled]" value="true"/>');
+      $('form.contact-form').append('<input type="hidden" name="contact[referrer]" value="' + document.referrer + '"/>');
 
-    $('details').details();
-    $('html').addClass($.fn.details.support ? 'details' : 'no-details');
-}
+      $('details').details();
+      $('html').addClass($.fn.details.support ? 'details' : 'no-details');
+  }
 
-$(GOVUK.feedback.init);
+  $(GOVUK.feedback.init);
+
+}());
