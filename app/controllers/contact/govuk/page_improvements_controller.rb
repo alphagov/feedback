88ba51ced5a4e469a2d10afe640a908df185a302
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-09-05 10:11:40 +0000

    Page improvements
    The page improvements endpoint is where the 'Improve this page' widget sends AJAX requests to send feedback to Zendesk. https://trello.com/c/HN1L3EfK/286-update-footer-feedback-component
    
    The feedback request is forwarded to the support-api which performs the guts of the task.

---
+++ app/controllers/contact/govuk/page_improvements_controller.rb
@@ -0,0 +1,15 @@
+class Contact::Govuk::PageImprovementsController < ApplicationController
+  def create
+    begin
+      Feedback.support_api.create_page_improvement(page_improvements_params)
+
+      render json: { status: 'success' }
+    rescue GdsApi::HTTPUnprocessableEntity => e
+      render json: e.error_details, status: e.code
+    end
+  end
+
+  def page_improvements_params
+    params.slice(:description, :email, :name, :path)
+  end
+end
