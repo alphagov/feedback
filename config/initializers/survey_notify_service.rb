12a75ae64747bf16d32dfeb337f47cac6a590ccc
Murray Steele <muz@h-lame.com>
2017-02-28 17:08:01 +0000

    Add an API Key for the notify service in test
    It's not a real API key that would actually work, but it does have the
    correct form so the notify client won't barf when trying to parse it out.
    
    We use webmock to stop any real requests in tests so a fake API key
    doesn't cause us any problems.

--- config/initializers/survey_notify_service.rb
+++ config/initializers/survey_notify_service.rb
@@ -1,5 +1,12 @@
 require 'survey_notify_service'
 
-api_key = ENV['SURVEY_NOTIFY_SERVICE_API_KEY']
+api_key = if Rails.env.test?
+  # This is not a valid key, but it has the right form so the client
+  # won't break when interrogating it
+  'testkey1-12345678-90ab-cdef-1234-567890abcdef-12345678-90ab-cdef-1234-567890abcdef'
+else
+  ENV['SURVEY_NOTIFY_SERVICE_API_KEY']
+end
+
 Feedback.survey_notify_service = SurveyNotifyService.new(api_key)
 
