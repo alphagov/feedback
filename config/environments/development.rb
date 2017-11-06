a1fc5a39229b9d9a239ee9633b7d64b07e140da3
Daniel Burnley <dan@danops.uk>
2017-11-06 16:16:50 +0000

    Clean up rails config
    As part of our work on upgrading rails we're also working to create
    a standard for rails configs going forward.
    
    The main things highlighted in this PR are:
    
    - load_defaults should always be first
    - Alphabetise config options
    - Group together relevant options (e.g. Assets options are all together)
    - Remove any default/unnecessary comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,37 +1,23 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
-
-  # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
+  config.action_mailer.raise_delivery_errors = false
 
-  # Do not compress assets
+  config.active_support.deprecation = :log
+
   config.assets.compress = false
-
-  # Expands the lines which load the assets
   config.assets.debug = true
-
   if ENV['GOVUK_ASSET_ROOT'].present?
     config.asset_host = ENV['GOVUK_ASSET_ROOT']
   end
 
+  config.cache_classes = false
+
+  config.consider_all_requests_local = true
+
   config.eager_load = false
+
+  config.whiny_nils = true
 end
