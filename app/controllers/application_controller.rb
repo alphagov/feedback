b6dbcb199f7b179ce6e1db4749346d550cdc57d0
Tijmen Brommet <tijmen@gmail.com>
2019-06-04 13:31:31 +0100

    Switch to default layout
    We're currently using the `wrapper` layout, which includes a lot of
    default styles from static. We're getting rid of the `wrapper` layout
    (in order to get rid of Static altogether). This makes this app use the
    default (core_layout).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   rescue_from GdsApi::BaseError, with: :unable_to_create_ticket_error
 
   include Slimmer::Template
-  slimmer_template 'wrapper'
 
 protected
 
