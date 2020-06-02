ee36a8b15ea9c6317d1dc608c06e431f5c4ae8c3
Kevin Dew <kevindew@me.com>
2020-06-02 19:27:22 +0100

    Remove unnecessary configuration
    I don't think anyone uses the STATIC_DEV env var and the Plek option is
    the default.

--- config/initializers/slimmer.rb
+++ config/initializers/slimmer.rb
@@ -1,7 +1,3 @@
 Rails.application.configure do
   config.slimmer.logger = Rails.logger
-
-  if Rails.env.development?
-    config.slimmer.asset_host = ENV["STATIC_DEV"] || Plek.current.find("static")
-  end
 end
