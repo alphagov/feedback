79ace7ba623c74eabc853b3032ab41d312109ffe
Celia Collins <celiadcollins@gmail.com>
2020-07-22 16:37:01 +0100

    Fix rubocop errors

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -60,7 +60,9 @@ private
   end
 
   def respond_to_valid_submission(ticket)
+    # rubocop:disable Rails/SaveBang
     ticket.save
+    # rubocop:enable Rails/SaveBang
     confirm_submission
   end
 
