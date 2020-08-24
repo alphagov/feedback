688c2f141f7d39120015bc94d77db0fbee31683f
Andrew White <andrew.white@unboxed.co>
2020-08-24 13:01:17 +0100

    Remove rack-cache config
    The rack-cache gem has not been enabled by default since Rails 4.0.

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,12 @@ module Feedback
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.action_dispatch.rack_cache = nil
 
+    # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
     config.assets.enabled = true
