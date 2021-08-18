8c1205fc581a437b656c277989bb230acb47feda
Kevin Dew <kevindew@me.com>
2021-08-18 19:46:17 +0100

    Removed unused jQuery extension
    There is no longer any code in this app that uses the 'txtinput' event
    that this jQuery extension introduced.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-//= require vendor/jquery.inputevent
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/character-count
