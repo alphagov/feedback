941f7195ed25ca81211988910c5a2ca417e31a95
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-09-22 12:16:15 +0100

    Allow optional questions to be skipped if required params are not present
    This commmit introuduces the concept of optional questions which only appear
    if certain conditions are met. If the required param key maps to the required
    value the optional question will be displayed. If not, the question is skipped
    and the next question rendered.

--- spec/controllers/contact/govuk/accessible_format_requests_controller_spec.rb
+++ spec/controllers/contact/govuk/accessible_format_requests_controller_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe Contact::Govuk::AccessibleFormatRequestsController, type: :contro
     let(:content_id) { "123abc" }
     let(:attachment_id) { "456def" }
     let(:base_params) { { content_id: content_id, attachment_id: attachment_id } }
-    let(:example_param) { "example" }
+    let(:example_param) { "custom" }
+    let(:optional_question) { format_request_questions.detect { |question| question["optional"] } }
+    let(:trigger_value) { optional_question["trigger-value"] }
+    let(:trigger_key) { optional_question["trigger-key"] }
 
     context "on the first question" do
       it "renders the first question title" do
@@ -18,7 +21,7 @@ RSpec.describe Contact::Govuk::AccessibleFormatRequestsController, type: :contro
     end
 
     context "on the 2nd question" do
-      before { post :form, params: base_params.merge({ 'question_number': "2", 'format_type': example_param }) }
+      before { post :form, params: base_params.merge({ 'question_number': "2", "#{trigger_key}": trigger_value }) }
       it "renders the 2nd question title" do
         expect(response.body).to include(format_request_questions[1]["title"])
       end
@@ -32,6 +35,37 @@ RSpec.describe Contact::Govuk::AccessibleFormatRequestsController, type: :contro
 
       it "increments the previous question number as a hidden input" do
         expect(response.body).to have_css("input[name=question_number][value=3]", visible: false)
+      end
+    end
+
+    context "on an optional question" do
+      let(:optional_question_index) { format_request_questions.find_index(optional_question) }
+      let(:optional_question_number) { optional_question_index + 1 }
+
+      describe "when the optional triggers are passed in the params" do
+        before { post :form, params: base_params.merge({ 'question_number': optional_question_number, "#{trigger_key}": trigger_value }) }
+        it "renders the optional question" do
+          expect(response.body).to include(optional_question["title"])
+        end
+
+        it "maintains the request question_number params in the response" do
+          expect(request.params["question_number"]).to eq optional_question_number.to_s
+          expect(controller.params["question_number"]).to eq optional_question_number.to_s
+        end
+      end
+
+      describe "when the optional trigger is not passed in the params" do
+        let(:next_question_index) { optional_question_index + 1 }
+        before { post :form, params: base_params.merge({ 'question_number': optional_question_number, "#{trigger_key}": "example" }) }
+
+        it "skips the optional question and renders the next question" do
+          expect(response.body).to include(format_request_questions[next_question_index]["title"])
+        end
+
+        it "increments the request question_number param in the response" do
+          expect(request.params["question_number"]).to eq optional_question_number.to_s
+          expect(controller.params["question_number"]).to eq (optional_question_number + 1).to_s
+        end
       end
     end
 
