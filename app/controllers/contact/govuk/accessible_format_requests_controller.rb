70c54a86c877d0cef4e0dce7e51ec679ed39345c
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-09-22 12:16:15 +0100

    Allow optional questions to be skipped if required params are not present
    This commmit introuduces the concept of optional questions which only appear
    if certain conditions are met. If the required param key maps to the required
    value the optional question will be displayed. If not, the question is skipped
    and the next question rendered.

--- app/controllers/contact/govuk/accessible_format_requests_controller.rb
+++ app/controllers/contact/govuk/accessible_format_requests_controller.rb
@@ -1,5 +1,6 @@
 class Contact::Govuk::AccessibleFormatRequestsController < ContactController
   helper_method :question_component, :question_title, :question_caption, :question_inputs, :question_options, :question_key, :last_question?, :next_question_number, :permitted_params
+  before_action :increment_question_number_if_optional_skipped, only: [:form]
 
   def form; end
 
@@ -10,7 +11,17 @@ private
   end
 
   def current_question
-    questions[question_number - 1]
+    @current_question ||= questions[question_number - 1]
+  end
+
+  def next_question
+    last_question? ? questions.last : questions[question_number]
+  end
+
+  def presented_question
+    return current_question unless optional_question?
+
+    @presented_question ||= display_optional_question? ? current_question : next_question
   end
 
   def question_component
@@ -18,15 +29,15 @@ private
   end
 
   def question_title
-    current_question[:title]
+    presented_question[:title]
   end
 
   def question_caption
-    current_question[:caption]
+    presented_question[:caption]
   end
 
   def question_inputs
-    current_question[:inputs]
+    presented_question[:inputs]
   end
 
   def question_options
@@ -35,6 +46,12 @@ private
 
   def question_key
     question_inputs[0][:key]
+  end
+
+  def increment_question_number_if_optional_skipped
+    if optional_question? && !display_optional_question?
+      params[:question_number] = (params[:question_number].to_i + 1).to_s
+    end
   end
 
   def question_number
@@ -54,5 +71,15 @@ private
     document_params = %i[content_id attachment_id]
     question_keys = questions.map { |q| q["inputs"].pluck(:key) }.flatten
     params.permit(*question_keys, *document_params)
+  end
+
+  def optional_question?
+    current_question[:optional]
+  end
+
+  def display_optional_question?
+    trigger_key = current_question[:"trigger-key"]
+    trigger_value = current_question[:"trigger-value"]
+    params[trigger_key] == trigger_value
   end
 end
