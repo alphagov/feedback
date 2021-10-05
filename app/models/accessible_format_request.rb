fe1e02c62a25d79dfa614596edf72c9c0c9f0357
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-10-05 10:53:03 +0100

    Add initial AccessibleFormatRequest Model with validations
    The AccessibleFormatRequest model will be created with
    several required attributes without which the model will
    not be able to Save

---
+++ app/models/accessible_format_request.rb
@@ -0,0 +1,20 @@
+class AccessibleFormatRequest
+  include ActiveModel::Validations
+  attr_accessor :document_title, :publication_path, :format_type, :custom_details, :contact_name, :alternative_format_email, :contact_email
+
+  validates :document_title, presence: true
+  validates :publication_path, presence: true
+  validates :format_type, presence: true
+  validates :alternative_format_email,
+            presence: true,
+            email: { message: "The email address must be valid" }
+  validates :contact_email,
+            presence: true,
+            email: { message: "The email address must be valid" }
+
+  def initialize(attributes = {})
+    attributes.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+end
