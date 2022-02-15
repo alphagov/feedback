0b4628815171392b292b05bdd15f11d6d77dece2
Andy Driver <andy@pagezero.net>
2022-02-15 10:14:16 +0000

    Add Javascript tests
    As part of enabling continuous deployment, the feedback app must be brought up
    to a minimum level of safety checks - this includes at least one Javascript test
    as JS is in use.
    
    Setting up Jasmine tests with the `rails generate jasmine:install` command
    created most of the files and directory structure, but
    spec/javascripts/support/jasmine_helper.rb is taken from
    govuk_publishing_components.
    
    Gemfile and Rakefile modifications were copied from email-alert-frontend.

---
+++ spec/javascripts/spec/feedback.spec.js
@@ -0,0 +1,99 @@
+describe('Feedback', function () {
+  'use strict'
+
+  it('can extract a path from a URL', function () {
+    expect(GOVUK.feedback.getPathFor('http://example.com/this/is/the/path')).toBe('/this/is/the/path')
+  })
+
+  describe('when on the contact page', function () {
+    var locationPathnameSpy
+
+    beforeEach(function () {
+      locationPathnameSpy = spyOn(GOVUK.feedback, 'getLocationPathname').and.returnValue('/contact')
+      GOVUK.feedback.init()
+    })
+
+    afterEach(function () {
+      locationPathnameSpy.and.callThrough()
+    })
+
+    it('stores the referrer in a cookie', function () {
+      expect(GOVUK.getCookie('govuk_contact_referrer')).toBe(document.referrer)
+    })
+  })
+
+  describe('when there is a feedback form on the page', function () {
+    var contactForm, linkInput, specificLocationInput, javascriptEnabledInput, referrerInput
+
+    beforeAll(function () {
+      contactForm = document.createElement('form')
+      contactForm.setAttribute('class', 'contact-form')
+      linkInput = document.createElement('input')
+      linkInput.setAttribute('name', 'contact[link]')
+      contactForm.appendChild(linkInput)
+      specificLocationInput = document.createElement('input')
+      specificLocationInput.setAttribute('name', 'contact[location]')
+      specificLocationInput.setAttribute('value', 'specific')
+      specificLocationInput.setAttribute('type', 'checkbox')
+      contactForm.appendChild(specificLocationInput)
+      document.body.appendChild(contactForm)
+    })
+
+    beforeEach(function () {
+      linkInput.value = ''
+      specificLocationInput.checked = false
+      GOVUK.feedback.init()
+      javascriptEnabledInput = contactForm.querySelector('[name="contact[javascript_enabled]"]')
+      referrerInput = contactForm.querySelector('[name="contact[referrer]"]')
+    })
+
+    afterEach(function () {
+      javascriptEnabledInput.remove()
+      referrerInput.remove()
+    })
+
+    it('appends a hidden field indicating javascript was enabled', function () {
+      expect(javascriptEnabledInput.value).toBe('true')
+    })
+
+    it('appends a hidden field recording the referrer', function () {
+      expect(referrerInput.value).toBe(document.referrer)
+    })
+
+    describe('when no link URL is given', function () {
+      it('uses the referrer URL', function () {
+        expect(linkInput.value).toBe(document.referrer)
+      })
+    })
+
+    describe('when a link URL is given', function () {
+      beforeEach(function () {
+        linkInput.value = 'http://example.com'
+        GOVUK.feedback.init()
+      })
+
+      it('selects the "A specific page" option', function () {
+        expect(specificLocationInput.checked).toBe(true)
+      })
+    })
+
+    describe('when there is an email address in the specific page URL', function () {
+      var cookieSpy
+
+      beforeEach(function () {
+        linkInput.value = ''
+        var referrerWithEmail = document.referrer + '?email=test@example.com'
+        cookieSpy = spyOn(GOVUK, 'cookie').and.returnValue(referrerWithEmail)
+        GOVUK.feedback.init()
+      })
+
+      afterEach(function () {
+        cookieSpy.and.callThrough()
+      })
+
+      it('masks all email addresses in the URL', function () {
+        expect(linkInput.value).toBe(document.referrer + '?email=[email]')
+      })
+    })
+  })
+})
