79916bdbd6d4b121f487919fdf71077ebd9d5fb5
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-07 17:02:11 +0100

    Remove button component JavaScript
    This is now included in `static`'s JavaScript, so can be removed from this app.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/error-summary
