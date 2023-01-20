04563929b192ccee610b037146cb02bccba5af5d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2023-01-20 15:27:15 +0000

    Get completed_transaction content item data
    The content on the forms (such as the title and whether to display a promo box) is rendered dynamically from the content item. Therefore, a call needed to be made to Content Store to retrieve the correct content to render.
    
    When the form was in Frontend, it submitted the data to the Feedback app, so the controllers already existed. They all inherit from ContactController, so the #new action was overwritten in the subclasses to render @Publication. @Publication stores the presented content item (fetched from the Content Store if it isn’t already cached). Fetching and formatting the content item is handled by ServiceFeedbackHelper and ContentItemPresenter.

---
+++ app/presenters/content_item_presenter.rb
@@ -0,0 +1,43 @@
+class ContentItemPresenter
+  attr_reader :content_item
+
+  def initialize(content_item)
+    @content_item = content_item
+  end
+
+  PASS_THROUGH_KEYS = %i[
+    base_path content_id details description locale title
+  ].freeze
+
+  PASS_THROUGH_DETAILS_KEYS = [
+    :promotion,
+  ].freeze
+
+  PASS_THROUGH_KEYS.each do |key|
+    define_method key do
+      content_item[key.to_s]
+    end
+  end
+
+  PASS_THROUGH_DETAILS_KEYS.each do |key|
+    define_method key do
+      details[key.to_s] if details
+    end
+  end
+
+  def slug
+    URI.parse(base_path).path.sub(%r{\A/}, "")
+  end
+
+  def format
+    @content_item["schema_name"]
+  end
+
+  def short_description
+    nil
+  end
+
+  def web_url
+    Plek.new.website_root + base_path
+  end
+end
