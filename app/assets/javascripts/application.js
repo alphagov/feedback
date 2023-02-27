59cc38383dd63e2e2f884731d07e563fceee8b28
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2023-02-27 18:26:52 +0000

    Improve styling with design system components
    Examples to try:
    
    Rendering the step by step nav and header
    http://feedback.dev.gov.uk/done/find-pension-contact-details
    
    Rendering the footer
    http://feedback.dev.gov.uk/done/vehicle-tax
    
    Rendering the side nav
    https://www.gov.uk/done/transaction-finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/step-by-step-nav
 
 (function () {
   'use strict'
