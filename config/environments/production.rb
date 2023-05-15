53b9a13f5c4cb95d0ba824ea580166f71b153e46
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-05-15 12:48:39 +0100

    Enable the Rails cache, backed by memcache.
    Same as other frontend apps. Feedback will initially use this for
    ephemeral data for the Rack::Attack rate-limiting feature on the
    feedback form. See parent commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Prefix memcache keys so that apps can optionally share a cache.
+  config.cache_store = :mem_cache_store, nil, { namespace: :feedback, compress: true }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
