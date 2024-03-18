05930a49c2cdb803b1f480c7a84cc9ae86c2fa4b
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2024-03-18 15:51:44 +0000

    Move Javascript modules to es6-components
    To prevent browsers evaluating JS that isn't supported, components that contain code from govuk-frontend have been moved to a separate file `es6-components.js` which is included in `application.html.erb` as a script tag with `type="module"`

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/details
-//= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/step-by-step-nav
 
 (function () {
