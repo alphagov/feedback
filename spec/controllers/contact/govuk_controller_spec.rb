2fe7812e97b110686a19728e922b6a49b58b7bc7
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-08-28 11:04:59 +0100

    explicit stub in controller spec

--- spec/controllers/contact/govuk_controller_spec.rb
+++ spec/controllers/contact/govuk_controller_spec.rb
@@ -17,11 +17,30 @@ RSpec.describe Contact::GovukController, type: :controller do
     }
   end
 
+  let(:ticket_creator_params) do
+    {
+      details: "Testing, testing, 1, 2, 3...",
+      link: nil,
+      user_specified_url: nil,
+      user_agent: "Rails Testing",
+      referrer: nil,
+      javascript_enabled: false,
+      url: nil,
+      path: nil,
+      requester: {
+        name: "Joe Bloggs",
+        email: "test@test.com",
+      },
+    }
+  end
+
+  let(:support_ticket) { SupportTicketCreator.new(ticket_creator_params) }
+
   it_behaves_like "a GOV.UK contact"
 
   context "with a valid contact submission" do
     it "should pass the contact onto the support api" do
-      stub_post = stub_any_support_api_call
+      stub_post = stub_support_api_valid_raise_support_ticket(support_ticket.payload)
 
       post :create, params: valid_params
 
