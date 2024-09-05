5388432bf67c68cf9385c36add41e66c507e6abf
AshGDS <8880610+AshGDS@users.noreply.github.com>
2024-09-05 11:51:19 +0100

    Move es6-components.js back into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/character-count
+//= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/step-by-step-nav
-
 (function () {
   'use strict'
 
