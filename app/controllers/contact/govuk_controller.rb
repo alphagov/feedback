98bb538f435721f955784dc95aca52f3fcd8ed6c
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-01-30 11:44:54 +0000

    Limit 'Thank you' GA4 tracking to GOVUK contact form only
    Multiple forms end up on the same 'Thank you' page, however analysts are only interested in tracking the page when the origin is the GOVUK contact form. Therefore I've added some logic to prevent other forms like service feedback forms from triggering the GA4 tracking

--- app/controllers/contact/govuk_controller.rb
+++ app/controllers/contact/govuk_controller.rb
@@ -5,12 +5,17 @@ class Contact::GovukController < ContactController
     hide_report_a_problem_form_in_response
   },
                 only: %i[anonymous_feedback_thankyou named_contact_thankyou]
+  before_action :check_govuk_contact_form
 
   def anonymous_feedback_thankyou; end
 
   def named_contact_thankyou; end
 
 private
+
+  def check_govuk_contact_form
+    @govuk_contact_form = params[:govuk_contact_form] == "true"
+  end
 
   def ticket_class
     ContactTicket
