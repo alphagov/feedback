f72d9af058de965962b1691f2d7b358ba4721ccf
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-03-31 10:47:23 +0100

    Add basic elements bundle
    - although we could bring in these elements as individual stylesheets, in
      email-alert-frontend we got considerable benefit by bundling everything
      that was used in all pages on the app, so we start off by doing that here.
    - basic component stylesheets in application.scss
    - anything in application.scss also needs to be added to the custom
      exclude list so that it isn't included twice by the components gem.
    - finally, the JS files needed for those components (previously supplied
      by static)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,15 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/layout-super-navigation-header
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/skip-link
 //= require govuk_publishing_components/components/step-by-step-nav
+
 (function () {
   'use strict'
 
