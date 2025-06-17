ceea10cf6e4cc68a943d81f4f6450b4e13be7f7b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-06-17 14:47:44 +0100

    Remove unneeded style imports
    - feedback uses per-page asset loading, which means components already load their own stylesheets, so no need to manually import them in application.scss
    - completed transaction stylesheet was also importing things it didn't use (individual component support is for component style mixins etc.)
    - also set configuration to not exclude styles from static, since feedback doesn't use static anymore and should load all its own CSS

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -1,17 +1,5 @@
 GovukPublishingComponents.configure do |c|
   c.component_guide_title = "Email Alert Frontend Component Guide"
   c.application_stylesheet = "application"
-  c.custom_css_exclude_list = %w[
-    button
-    cookie-banner
-    heading
-    input
-    label
-    layout-footer
-    layout-for-public
-    layout-super-navigation-header
-    search
-    search-with-autocomplete
-    skip-link
-  ]
+  c.exclude_css_from_static = false
 end
